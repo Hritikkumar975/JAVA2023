098d95f67d35825491a23e40ab9cfecdf4a916ab
Udaybisht124 <96695491+Udaybisht124@users.noreply.github.com>
2023-11-13 22:06:43 +0530

    7017package9done

---
+++ 7017package9one.java
@@ -0,0 +1,10 @@
+
+package c4.sample;
+
+ class SampleClass {
+    protected int protectedVar = 20;
+    protected void protectedMethod() {
+        System.out.println("Protected Method");
+        System.out.println("Protected Var = " + protectedVar);
+    }
+}
---
+++ 7017package9two.java
@@ -0,0 +1,11 @@
+
+import c4.sample.SampleClass;
+class AccessValues extends SampleClass{
+    public static void main(String[] args) {
+        AccessValues obj=new AccessValues();
+        obj.protectedMethod();
+ 
+    }
+
+    
+}
