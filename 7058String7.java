672597c36d6363aa481f7d08b9dbf9c0dc30e8ca
Aryan chugh <106946674+aryanabhi12@users.noreply.github.com>
2023-11-14 12:40:32 +0530

    Add files via upload

---
+++ 7058String7.java
@@ -0,0 +1,15 @@
+// String:- 7. Program to add characters to a String.     By(7058/21)
+
+import java.io.*;
+
+class Addchtostr{
+   public static void main(String args[]){
+      char a='H';
+      char l='b';
+String str1="Aryan chugh ";
+String str2=str1+a+l;
+
+System.out.println("String is:"+str1);
+System.out.println("String after adding characters is:"+str2);
+}
+}
