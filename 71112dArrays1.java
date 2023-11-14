5d8c3599728b34342de7721c79228c4df38993f1
Jatin <jatinnegi983@gmail.com>
2023-11-14 13:32:49 +0530

    Jaspreet Singh 7111

---
+++ 71112dArrays1.java
@@ -0,0 +1,31 @@
+import java.util.Scanner;
+class MatrixInputAndPrint {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Enter the number of rows: ");
+        int rows = scanner.nextInt();
+
+        System.out.print("Enter the number of columns: ");
+        int columns = scanner.nextInt();
+
+        int[][] matrix = new int[rows][columns];
+
+        System.out.println("Enter the matrix elements:");
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                System.out.print("Enter element at position [" + i + "][" + j + "]: ");
+                matrix[i][j] = scanner.nextInt();
+            }
+        }
+
+        System.out.println("Matrix:");
+
+        for (int i = 0; i < rows; i++) {
+            for (int j = 0; j < columns; j++) {
+                System.out.print(matrix[i][j] + "\t");
+            }
+            System.out.println(); 
+        }
+    }
+}
