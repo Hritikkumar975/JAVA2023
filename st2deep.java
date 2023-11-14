af30ba40e0796679ca001847c2ee0170f9f5e1c7
Punit2213 <punit22joshi@gmail.com>
2023-11-14 10:30:07 +0530

    Add *.java

---
+++ st2deep.java
@@ -0,0 +1,16 @@
+import java.util.*;
+
+public class st2deEp
+{
+    public static void main(String[] args)
+    {
+        int[] vals={3,7,33,56};
+        Ex e=new Ex(vals);
+        e.showData();
+        vals[0]=13;
+        e.showData;
+        
+        
+        
+    }
+}
