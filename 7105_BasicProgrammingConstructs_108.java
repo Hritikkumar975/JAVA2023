181c8f213359b35e5e9d7f3e6be8d0531d0da367
kbhavneet03 <130518259+kbhavneet03@users.noreply.github.com>
2023-11-16 12:34:22 +0530

    7105/21 Bhavneet Kaur

---
+++ 7105_BasicProgrammingConstructs_108.java
@@ -0,0 +1,15 @@
+
+class O {
+	public static void main(String[] args)
+	{
+		
+		int N = 7;
+
+		
+		for (int i = 1; i <= 10; i++) {
+			
+			System.out.println(N + " * " + i + " = "
+							+ N * i);
+		}
+	}
+}
